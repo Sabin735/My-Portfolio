41f55838cc256febc6c1c800819627c80db1c5ff
Sabin735 <sabin72517@gmail.com>
2026-07-26 21:28:52 +0545

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
 function showSlides(n) {
   let i;
@@ -18,11 +15,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+
   slides[slideIndex-1].style.display = "block";
-  if (dots.length > 0) {
-    dots[slideIndex-1].className += " active";
-  }
+
 }
